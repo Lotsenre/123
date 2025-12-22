4ecfd75616eb21989578f3fe4a44aece45c31e58
Andrew766938 <rnbrc122@gmail.com>
2025-12-22 09:28:29 +0300

    Add models init file with imports

---
+++ app/models/__init__.py
@@ -0,0 +1,19 @@
+from app.models.tickets import (
+    Train,
+    Wagon,
+    Seat,
+    Ticket,
+    TrainType,
+    DiscountType,
+    Base,
+)
+
+__all__ = [
+    "Train",
+    "Wagon",
+    "Seat",
+    "Ticket",
+    "TrainType",
+    "DiscountType",
+    "Base",
+]
